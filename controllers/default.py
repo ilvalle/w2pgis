7c0c24accba0245692156b09dd6f66e76b0a4cf3
ilvalle <paolo.valleri@gmail.com>
2013-11-15 16:01:38 +0100

    removed a few useless files

--- controllers/default.py
+++ controllers/default.py
@@ -1,69 +1,16 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a samples controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
 from gluon.serializers import loads_json
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
+    return {}
 
-    if you need a simple wiki simple replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
-
-#import json
 def get_geojson():
-    rows= db(db.route).select(db.route.name, db.route.geo.st_asgeojson())
-#    features= [json.loads(r[db.route.geo.st_asgeojson()]) for r in rows] 
+    rows= db(db.geostuff).select(db.geostuff.name, db.geostuff.geometry.st_asgeojson())
+
     features= [{"type": "Feature",
                 "properties": {
-                    "popupContent": r[db.route.name]
+                    "popupContent": r[db.geostuff.name]
                 },
-                "geometry": loads_json(r[db.route.geo.st_asgeojson()])} for r in rows] 
+                "geometry": loads_json(r[db.geostuff.geometry.st_asgeojson()])} for r in rows] 
 
     return response.json({"type": "FeatureCollection", 'features': features})
-
-def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
-
-
-def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
-
-
-def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    return service()
-
--- models/db.py
+++ models/db.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 
-#db = DAL('postgres://ilvalle:web2py@postgres.server:5432/ilvalle$default')
 
-#db.define_table('geostuff',
-#    Field('name', 'string'),
-#    Field('geometry', 'geometry()'),
-#)
 
