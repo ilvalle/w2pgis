481a1ba61ee7562106e9c71b67f93fd0f7de2b3e
ilvalle <paolo.valleri@gmail.com>
2013-11-15 15:36:24 +0100

    Initial import

---
+++ models/db.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+#db = DAL('postgres://ilvalle:web2py@postgres.server:5432/ilvalle$default')
+
+#db.define_table('geostuff',
+#    Field('name', 'string'),
+#    Field('geometry', 'geometry()'),
+#)
+
